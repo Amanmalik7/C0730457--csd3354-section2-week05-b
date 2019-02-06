60fe5257c305995c1b71e45a7669f8ce29ae5c56
visualstudio <visualstudio@122-PC13>
2019-02-06 11:41:01 -0500

    Add project files.

---
+++ C0730457 linked lists  1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0730457_linked_lists__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
